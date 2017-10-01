9c681981411581bf8cad109d235b5e959540df65
Eugene Levit <eug.levit@gmail.com>
2017-09-30 23:10:23 -0400

    made click coin work

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+$(document).ready(function() {
+
 let aUrl = 'https://min-api.cryptocompare.com/data/histoday';
 let bUrl = 'https://api.coinmarketcap.com/v1/ticker';
 let cUrl = 'https://www.cryptocompare.com/api/data/coinlist'
@@ -5,50 +7,62 @@ let cUrl = 'https://www.cryptocompare.com/api/data/coinlist'
 
 
 function getCrypCompApi() {
-	let query = {
-		// ts: $.now(),
-		fsym: 'ETH',
-		tsym: 'USD',
+    let query = {
+        // ts: $.now(),
+        fsym: 'ETH',
+        tsym: 'USD',
 
-	}
-	$.getJSON(aUrl,query,getData);
+    }
+    $.getJSON(aUrl, query, getData);
 };
 
 
 
 function getCoinMarkCapApi() {
-	$.ajax({ 
-		type: 'GET', 
-		url: 'https://api.coinmarketcap.com/v1/ticker/?limit=10' 
-	}).done(renderCoinList);
+    $.ajax({
+        type: 'GET',
+        url: 'https://api.coinmarketcap.com/v1/ticker/?limit=10'
+    }).done(renderCoinList);
 }
 
 function renderCoinList(data) {
-	console.log(data);
-		data.forEach(name =>
-			$('.cryptoList').append(`
-    				<li class='cryptoCoin'>${name.id}</li>
-			`
-			)
-			)
- 
-
-    	
-		
+    console.log(data);
+    data.forEach(name =>
+        $('.cryptoList').append(`
+    				<li class='cryptoCoin'><button class='coin' val=${name.id}>${name.id}</button></li>
+			`)
+    )
 };
 
 
+
+
 function getData(data) {
-	console.log(Date(data.Data[0].time));
-	console.log(data.Data[0].close);
+    console.log(Date(data.Data[0].time));
+    console.log(data.Data[0].close);
 }
 
 function getCoins(data) {
-	console.log(data[1].id);
+    console.log(data[1].id);
 }
+
+function clickCoin() {
+$(document).on('click', '.coin', function(event) {
+	console.log('test');
+	event.preventDefault();
+	console.log($(this).attr('val'));
+})
+}
+
 
 getCrypCompApi();
 getCoinMarkCapApi();
+clickCoin();
+
+
+})
+
+
 // renderCoinList();
 
 //https://min-api.cryptocompare.com/data/all/exchanges
