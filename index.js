13ac8d4ecbe3b43daeff4fddef86218827847f65
Eugene Levit <eug.levit@gmail.com>
2017-09-30 22:28:21 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,76 @@
+let aUrl = 'https://min-api.cryptocompare.com/data/histoday';
+let bUrl = 'https://api.coinmarketcap.com/v1/ticker';
+let cUrl = 'https://www.cryptocompare.com/api/data/coinlist'
+
+
+
+function getCrypCompApi() {
+	let query = {
+		// ts: $.now(),
+		fsym: 'ETH',
+		tsym: 'USD',
+
+	}
+	$.getJSON(aUrl,query,getData);
+};
+
+
+
+function getCoinMarkCapApi() {
+	$.ajax({ 
+		type: 'GET', 
+		url: 'https://api.coinmarketcap.com/v1/ticker/?limit=10' 
+	}).done(renderCoinList);
+}
+
+function renderCoinList(data) {
+	console.log(data);
+		data.forEach(name =>
+			$('.cryptoList').append(`
+    				<li class='cryptoCoin'>${name.id}</li>
+			`
+			)
+			)
+ 
+
+    	
+		
+};
+
+
+function getData(data) {
+	console.log(Date(data.Data[0].time));
+	console.log(data.Data[0].close);
+}
+
+function getCoins(data) {
+	console.log(data[1].id);
+}
+
+getCrypCompApi();
+getCoinMarkCapApi();
+// renderCoinList();
+
+//https://min-api.cryptocompare.com/data/all/exchanges
+
+// let aUrl = 'https://min-api.cryptocompare.com/data/histominute';
+// let searchTerm= {
+// 	fsym: 'ETH',
+// 	tsyms: 'BTC'
+
+// }
+
+// function setup() {
+// 	let query = {
+// 		url:'https://min-api.cryptocompare.com/data/histominute',
+// 		fsym: 'BTC',
+// 		tsym: 'USD'
+// 	}
+// 	$.getJSON(aUrl,query,getData);
+
+// 	};
+
+
+// function getData(data) {
+// 	console.log(data.Data[0]);
+// }
