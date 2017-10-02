cd4a1231a61f328ac5fb9d7f39772a6ebd7dc1c4
Eugene Levit <eug.levit@gmail.com>
2017-10-02 18:47:44 -0400

    mostly cosmetic updates

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
     function getCoinMarkCapApi() {
         $.ajax({
             type: 'GET',
-            url: 'https://api.coinmarketcap.com/v1/ticker/?limit=20'
+            url: 'https://api.coinmarketcap.com/v1/ticker/?limit=4'
         }).done(renderCoinList);
     }
 
@@ -15,15 +15,17 @@ $(document).ready(function() {
         console.log(data);
         data.forEach(name =>
             $('.cryptoList').append(`
-    				<button class='cryptoCoin coin' val=${name.symbol}>${name.id}</button>
+    				<button class='cryptoCoin coin' val=${name.symbol}>${name.id} (${name.symbol})</button>
 			`)
         );
     }
 
     function renderExchangeList(coinChoice) {
         $('.exchangeList').html('');
-        $('.exchangeList').html(`<span class='coin-choice-button'>Coin Name: ${coinChoice}</span>`);
         $('.exchangeList').css('height', '100vh');
+        $('.second-page-container').css('height', '100vh');
+        $('.exchangeList').html(`<span class='coin-choice-button'>Coin Name: ${coinChoice}</span>`);
+
         let total = [];
         console.log(coinChoice);
         $.getJSON(bUrl, function(data) {
@@ -41,14 +43,31 @@ $(document).ready(function() {
                     });
                 };
             };
+            console.log('beforebutton');
+            $('.exchangeList').append(`<button class='startOver'>Start Over</button>`);
+            console.log($('.startOver').val());
+            console.log('afterbutton');
+
         })
+    }
+
+    function startOver() {
+    	$(document).on('click', '.startOver', function(event){
+    		event.preventDefault();
+    		$('html, body').animate({
+            scrollTop: $(".first-page-container").offset().top
+        }, 1000);
+        setTimeout(function() {$('.exchangeList').html('')}, 850);
+        setTimeout(function() {$('.second-page-container, .exchangeList').css('height','0%')}, 900)
+    		
+    	})
     }
 
     function clickCoin(className) {
         console.log($(this).attr('val'));
         $('.search-button').val('');
         $('html, body').animate({
-            scrollTop: $(".exchangeList").offset().top
+            scrollTop: $(".second-page-container").offset().top
         }, 2000);
         
 
@@ -56,11 +75,47 @@ $(document).ready(function() {
 
     }
 
+ $(window).scroll(function() {
+  
+  // selectors
+  var $window = $(window),
+      $body = $('body'),
+      $panel = $('.panel');
+  
+  // Change 33% earlier than scroll position so colour is there when you arrive.
+  var scroll = $window.scrollTop() + ($window.height() / 3);
+ 
+  $panel.each(function () {
+    var $this = $(this);
+    
+    // if position is within range of this panel.
+    // So position of (position of top of div <= scroll position) && (position of bottom of div > scroll position).
+    // Remember we set the scroll to 33% earlier in scroll var.
+    if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
+          
+      // Remove all classes on body with color-
+      $body.removeClass(function (index, css) {
+        return (css.match (/(^|\s)color-\S+/g) || []).join(' ');
+      });
+       
+      // Add class of currently active div
+      $body.addClass('color-' + $(this).data('color'));
+    }
+  });    
+  
+}).scroll();
+
+ function moreCoins(){
+ 	
+ }
+
 
 
 
     getCoinMarkCapApi();
-    $(document).on('click', '.coin, .search-button', function(event) {
+    startOver();
+
+    $(document).on('click', '.cryptoCoin, .search-button', function(event) {
         event.preventDefault();
         let searching = $('.search-term').val().toUpperCase();
         $('.search-button').attr('val', searching);
@@ -74,7 +129,7 @@ $(document).ready(function() {
 
 
     //loading over and over again - done
-    //scroll back up
+    //scroll back up - done need styling
     //prices with the names
     //search button more prominent
     //filter out zero results
